e902211f55af009404232d3d1e39dd39ca621c4b
Raseel <raseelp321@gmail.com>
2024-11-01 00:20:54 +0530

    Update onbordeing.dart

--- lib/pages/onbordeing.dart
+++ lib/pages/onbordeing.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:glint/pages/phonenumber.dart';
 
 class Onbordeing extends StatefulWidget {
@@ -11,14 +12,26 @@ class Onbordeing extends StatefulWidget {
 class _OnbordeingState extends State<Onbordeing> {
   @override
   Widget build(BuildContext context) {
+    ScreenUtil.init(context);
     return Scaffold(
       body: Center(
         child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(17)),
+                padding:
+                    EdgeInsets.symmetric(horizontal: 100.w, vertical: 12.h),
+                elevation: 0,
+                backgroundColor: Colors.lightBlue[500],
+                foregroundColor: Colors.white),
             onPressed: () {
               Navigator.push(context,
                   MaterialPageRoute(builder: (context) => Phonenumber()));
             },
-            child: Text('Le5ts get started')),
+            child: const Text(
+              'Let\'s get started',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            )),
       ),
     );
   }
